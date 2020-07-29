f2607b47b1a771f7d7dc254e99671c7deb3f60e8
Generalov Maxim <generalov.maksim@gmail.com>
2020-07-29 23:25:59 +0200

    Update awx_provision.py

--- plugins/modules/awx_provision.py
+++ plugins/modules/awx_provision.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Copyright: (c) 2020, Maxim Generalov <maxim.generalov@sap.com>
+# Copyright: (c) 2020, Maxim Generalov <maxim.generalov@netdevopsx.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 ANSIBLE_METADATA = {
